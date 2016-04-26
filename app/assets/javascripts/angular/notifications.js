210312f56016f94cf09c8c1d3222cf1c7e2d549d
RaulGHT <raulhidalgo93@hotmail.com>
2016-04-27 07:09:30 +0800

    Added files via upload

--- app/assets/javascripts/angular/notifications.js
+++ app/assets/javascripts/angular/notifications.js
@@ -1,14 +1,21 @@
 angular.module("lf.notifications", [])
-	.controller('notificationsCtrl', function($scope, notifRes) {
+	.controller('notificationsCtrl', function($scope,$interval, notifRes) {
+    	//$scope.userId = $scope.post.user_id;
     	$scope.maxItems = 5;
     	$scope.notifs = notifRes.query({limit: $scope.maxItems});
     	$scope.notifs.$promise.then(
 				function(res) {
-					console.log(res);
-				},
-				function(err) { 
+					$interval(function(){
+			
+					$scope.notifs=notifRes.query({limit: $scope.maxItems}); 
+                    //$scope.notif=notifRes.get({id: $stateParams.id}); 
+			        },10000);
+    				},
+    				function(err) { 
 					console.log(err);
+				
 				});
+				
     	// TODO : repeat every 5s notifRes.query({limit: $scope.maxItems, last: $scope.notifs[0].id});
     	//
     	// TODO: use this in view "_navbar" dropdown:
